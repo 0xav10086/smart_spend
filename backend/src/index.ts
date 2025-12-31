9e80a5bc2954d16bc0a69405a3e0ec0c8c08d297
0xav10086 <2320334524@qq.com>
2025-12-31 10:41:58 +0800

    add: 前后端分离的架构以及完成，将实现用户登录

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('SmartSpend Backend API is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
